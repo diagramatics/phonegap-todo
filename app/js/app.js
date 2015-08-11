e03e3fa0344466d814ecdbe06135f44fab3ffdef
Steven Sinatra <ohai@diagramatics.me>
2015-08-11 10:00:44 +1000

    localStorage support

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,22 @@
-'use strict';
 /* global angular */
+'use strict';
 
 var app = angular.module('phonegapTodo', []);
 
-app.controller('TodoController', function() {
+app.controller('TodoController', ['$scope', function($scope) {
+  // this.todos = window.localStorage.getItem('todos') || [];
   this.todos = [];
+  var storedTodos = window.localStorage.getItem('todos');
+  if (storedTodos && storedTodos !== 'undefined') {
+    angular.copy(JSON.parse(storedTodos), this.todos);
+    storedTodos = undefined;
+  }
+
+  $scope.$watch(function() {
+    return this.todos;
+  }.bind(this), function(newValue, oldValue) {
+    window.localStorage.setItem('todos', JSON.stringify(this.todos));
+  }.bind(this), true);
 
   this.addTodo = function() {
     if (this.addTodo.field.match(/^\w/i)) {
@@ -12,7 +24,9 @@ app.controller('TodoController', function() {
         text: this.addTodo.field,
         deadline: '0'
       });
+
+      // Reset the field
       this.addTodo.field = '';
     }
   };
-});
+}]);
